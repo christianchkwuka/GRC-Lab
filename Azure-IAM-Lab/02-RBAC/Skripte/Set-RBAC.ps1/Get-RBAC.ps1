c1c439477c5d6988edbec23fe631c991f61aa7a2
christianchkwuka <145847285+christianchkwuka@users.noreply.github.com>
2025-08-26 22:28:19 +0100

    Update Get-RBAC.ps1

--- Azure-IAM-Lab/02-RBAC/Skripte/Set-RBAC.ps1/Get-RBAC.ps1
+++ Azure-IAM-Lab/02-RBAC/Skripte/Set-RBAC.ps1/Get-RBAC.ps1
@@ -1,1 +1,13 @@
+# Get-RBAC.ps1
+# Purpose: List RBAC assignments for a resource group
+
+param(
+    [string]$Scope
+)
+
+Write-Host "Getting RBAC assignments for $Scope"
+
+az role assignment list `
+  --scope $Scope `
+  --output table
 
